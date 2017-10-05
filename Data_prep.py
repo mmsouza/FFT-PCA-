22d5d343f7e17058fc5badadd084f906f99fa5f9
mm_souza <matheussouza_z@hotmail.com>
2017-10-05 01:35:42 -0300

    Small refectory

--- Data_prep.py
+++ Data_prep.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
-
-def import_data():
-    normal_data = pd.read_csv('C:/Users/Lais-WHart/Google Drive/UFRGS/Mestrado/Data mining/Normal_lessdata/normal_output.csv',
-                              names=["XMEAS_1", "XMEAS_2", "XMEAS_3", "XMEAS_4", "XMEAS_5", "XMEAS_6", "XMEAS_7",
+colNames = ["XMEAS_1", "XMEAS_2", "XMEAS_3", "XMEAS_4", "XMEAS_5", "XMEAS_6", "XMEAS_7",
                                      "XMEAS_8", "XMEAS_9", "XMEAS_10",
                                      "XMEAS_11", "XMEAS_12", "XMEAS_13", "XMEAS_14", "XMEAS_15", "XMEAS_16", "XMEAS_17",
                                      "XMEAS_18", "XMEAS_19",
@@ -11,37 +8,26 @@ def import_data():
                                      "XMEAS_27", "XMEAS_28",
                                      "XMEAS_29", "XMEAS_30", "XMEAS_31", "XMEAS_32", "XMEAS_33", "XMEAS_34", "XMEAS_35",
                                      "XMEAS_36", "XMEAS_37",
-                                     "XMEAS_38", "XMEAS_39", "XMEAS_40", "XMEAS_41"])
+                                     "XMEAS_38", "XMEAS_39", "XMEAS_40", "XMEAS_41"]
 
-    # normal_Idv = pd.read_csv('C:/Users/Lais - WHart/Google Drive/UFRGS/Mestrado/Data mining\Normal/normal_idv_output.csv')
-    normal_data['normal'] = 1
-    normal_data['failure'] = 0
+
+def import_data(Data_path):
+    normal_data = pd.read_csv(Data_path + 'normal_lessdata_output.csv',
+                              names=colNames)
 
     list_aux = []
 
     for x in range(24, 696 + 24, 24):
-        df_aux = pd.read_csv('C:/Users/Lais-WHart/Google Drive/UFRGS/Mestrado/Data mining/Failure/Fault2_' + str(x) + '.csv',
-                             names=["XMEAS_1", "XMEAS_2", "XMEAS_3", "XMEAS_4", "XMEAS_5", "XMEAS_6", "XMEAS_7",
-                                    "XMEAS_8", "XMEAS_9", "XMEAS_10",
-                                    "XMEAS_11", "XMEAS_12", "XMEAS_13", "XMEAS_14", "XMEAS_15", "XMEAS_16", "XMEAS_17",
-                                    "XMEAS_18", "XMEAS_19",
-                                    "XMEAS_20", "XMEAS_21", "XMEAS_22", "XMEAS_23", "XMEAS_24", "XMEAS_25", "XMEAS_26",
-                                    "XMEAS_27", "XMEAS_28",
-                                    "XMEAS_29", "XMEAS_30", "XMEAS_31", "XMEAS_32", "XMEAS_33", "XMEAS_34", "XMEAS_35",
-                                    "XMEAS_36", "XMEAS_37",
-                                    "XMEAS_38", "XMEAS_39", "XMEAS_40", "XMEAS_41"])
+        df_aux = pd.read_csv(Data_path + 'Fault2_' + str(x) + '.csv',
+                             names=colNames)
 
         list_aux.insert(len(list_aux), df_aux)
-        #print('C:/Users/matheus/Desktop/Fault_TEP/Fault1' + str(x))
 
     fault1_df = pd.concat(list_aux, ignore_index=True)
 
-    fault1_df['normal'] = 0
-    fault1_df['failure'] = 1
-
-    #print(fault1_df.describe())
-
     return normal_data, fault1_df
+
+
 
 
 if __name__ == "__main__":
--- pca_ANN.py
+++ pca_ANN.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, cohen_kappa_score
 import pandas as pd
 from keras.models import Sequential
@@ -7,139 +6,72 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
-from sklearn.pipeline import Pipeline
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import decomposition
 from sklearn.preprocessing import StandardScaler
-
+import Data_prep as dp
 import time
 
 
-
-seed = 7
-np.random.seed(seed)
-
-normal_data = pd.read_csv('C:/Users/Lais-WHart/Google Drive/UFRGS/Mestrado/Data mining/Normal_lessdata/normal_output.csv',
-                          names=["XMEAS_1", "XMEAS_2", "XMEAS_3", "XMEAS_4", "XMEAS_5", "XMEAS_6", "XMEAS_7", "XMEAS_8",
-                                 "XMEAS_9", "XMEAS_10",
-                                 "XMEAS_11", "XMEAS_12", "XMEAS_13", "XMEAS_14", "XMEAS_15", "XMEAS_16", "XMEAS_17",
-                                 "XMEAS_18", "XMEAS_19",
-                                 "XMEAS_20", "XMEAS_21", "XMEAS_22", "XMEAS_23", "XMEAS_24", "XMEAS_25", "XMEAS_26",
-                                 "XMEAS_27", "XMEAS_28",
-                                 "XMEAS_29", "XMEAS_30", "XMEAS_31", "XMEAS_32", "XMEAS_33", "XMEAS_34", "XMEAS_35",
-                                 "XMEAS_36", "XMEAS_37",
-                                 "XMEAS_38", "XMEAS_39", "XMEAS_40", "XMEAS_41"])
-
-
-
-list_aux = []
-
-for x in range(24, 696 + 24, 24):
-    df_aux = pd.read_csv('C:/Users/Lais-WHart/Google Drive/UFRGS/Mestrado/Data mining/Failure/Fault2_' + str(x) + '.csv',
-                         names=["XMEAS_1", "XMEAS_2", "XMEAS_3", "XMEAS_4", "XMEAS_5", "XMEAS_6", "XMEAS_7", "XMEAS_8",
-                                "XMEAS_9", "XMEAS_10",
-                                "XMEAS_11", "XMEAS_12", "XMEAS_13", "XMEAS_14", "XMEAS_15", "XMEAS_16", "XMEAS_17",
-                                "XMEAS_18", "XMEAS_19",
-                                "XMEAS_20", "XMEAS_21", "XMEAS_22", "XMEAS_23", "XMEAS_24", "XMEAS_25", "XMEAS_26",
-                                "XMEAS_27", "XMEAS_28",
-                                "XMEAS_29", "XMEAS_30", "XMEAS_31", "XMEAS_32", "XMEAS_33", "XMEAS_34", "XMEAS_35",
-                                "XMEAS_36", "XMEAS_37",
-                                "XMEAS_38", "XMEAS_39", "XMEAS_40", "XMEAS_41"])
-
-    list_aux.insert(len(list_aux), df_aux)
-    print('C:/Users/matheus/Desktop/Fault_TEP/Fault1' + str(x))
-
-Fault1_df = pd.concat(list_aux, ignore_index=True)
-
-
-
-full_df = normal_data.append(Fault1_df, ignore_index=True)
-scaler  = StandardScaler().fit(full_df)
-
-
-full_df = pd.DataFrame(data=scaler.transform(full_df), columns=["XMEAS_1", "XMEAS_2", "XMEAS_3", "XMEAS_4", "XMEAS_5", "XMEAS_6", "XMEAS_7", "XMEAS_8",
-                                "XMEAS_9", "XMEAS_10",
-                                "XMEAS_11", "XMEAS_12", "XMEAS_13", "XMEAS_14", "XMEAS_15", "XMEAS_16", "XMEAS_17",
-                                "XMEAS_18", "XMEAS_19",
-                                "XMEAS_20", "XMEAS_21", "XMEAS_22", "XMEAS_23", "XMEAS_24", "XMEAS_25", "XMEAS_26",
-                                "XMEAS_27", "XMEAS_28",
-                                "XMEAS_29", "XMEAS_30", "XMEAS_31", "XMEAS_32", "XMEAS_33", "XMEAS_34", "XMEAS_35",
-                                "XMEAS_36", "XMEAS_37",
-                                "XMEAS_38", "XMEAS_39", "XMEAS_40", "XMEAS_41"])
-
-
-
-
-full_df = full_df.sample(frac=1).reset_index(drop=True)
-
-tic = time.clock()
-
-
-pca = decomposition.PCA(n_components=12)
-pca.fit(full_df)
-
-names = []
-
-for j in range(0, pca.n_components):
-    names.insert(len(names), "PC" + str(j))
-
-
-dfnormal= pd.DataFrame(data=pca.transform(normal_data), columns=names)
-dffailure= pd.DataFrame(data=pca.transform(Fault1_df), columns=names)
-
-dffailure['normal'] = 0
-dffailure['failure'] = 1
-dfnormal['normal'] = 1
-dfnormal['failure'] = 0
-
-full_df = dfnormal.append(dffailure, ignore_index=True)
-
-full_df = full_df.sample(frac=1).reset_index(drop=True)
-
-
-# Specify the data
-X = full_df.iloc[:, 0:12].astype(float)
-
-# Specify the target labels and flatten the array
-y = np_utils.to_categorical(full_df.iloc[:, 13:14])
-
-
 def baseline_model():
-    # create model
     model = Sequential()
     model.add(Dense(70, input_dim=12, activation='tanh'))
     model.add(Dense(40, activation='tanh'))
     model.add(Dense(15, activation='tanh'))
     model.add(Dense(2, activation='softmax'))
-    # Compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
 
-estimator = KerasClassifier(build_fn=baseline_model, epochs=20, batch_size=32, verbose=0)
+# -------------loading data-----------
+normal_data, Fault1_df = dp.import_data('C:/Users/matheus/Google Drive/UFRGS/Mestrado/Data mining/Data/')
+
+full_df = normal_data.append(Fault1_df, ignore_index=True)
+scaler = StandardScaler().fit(full_df)  # setup normalizer
+full_df = pd.DataFrame(data=scaler.transform(full_df), columns=dp.colNames)  # formatting result as pandas dataframe
+full_df = full_df.sample(frac=1).reset_index(drop=True)  # data shuffle
+
+tic = time.clock()  # time counter
+
+pca = decomposition.PCA(n_components=12)  # setting number of principle components
+pca.fit(full_df)
+
+# ----------------- Applying pca and formatting as pandas dataframe ------------
+names = []
+for j in range(0, pca.n_components):
+    names.insert(len(names), "PC" + str(j))
+
+dfnormal = pd.DataFrame(data=pca.transform(normal_data), columns=names)
+dffailure = pd.DataFrame(data=pca.transform(Fault1_df), columns=names)
+# ------------------------------------------------------------------------------
+
+# Adding dummy data, labels that mark if a given occurrence is normal or a failure
+dffailure['normal'] = 0
+dffailure['failure'] = 1
+dfnormal['normal'] = 1
+dfnormal['failure'] = 0
+
+# join both data classes
+full_df = dfnormal.append(dffailure, ignore_index=True)
+full_df = full_df.sample(frac=1).reset_index(drop=True)
+
+# Specify the data
+X = full_df.iloc[:, 0:12].astype(float)
+# Specify the target labels and flatten the array
+y = np_utils.to_categorical(full_df.iloc[:, 13:14])
+
+# setup classifier
+estimator = KerasClassifier(build_fn=baseline_model(), epochs=20, batch_size=32, verbose=0)
 estimator.fit(np.array(X), np.array(y))
 
-
-#y_pred = estimator.predict(X_test)
-
-
+# setup cross folder validation
+seed = 7
+np.random.seed(seed)
 kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
-
 results = cross_val_score(estimator, np.array(X), np.array(y), cv=kfold)
 print("Baseline: %.2f%% (%.2f%%)" % (results.mean() * 100, results.std() * 100))
 
+# print elapsed time
 toc = time.clock()
 print(toc - tic)
-
-'''
-print(precision_score(y_test, dfyf))
-#Recall
-print(recall_score(y_test, dfyf))
-
-# F1 score
-#f1_score(y_test, y_pred)
-
-# Cohen's kappa
-#cohen_kappa_score(y_test, y_pred)
-'''
