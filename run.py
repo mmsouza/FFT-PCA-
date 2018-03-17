a95c68ea50d18d226708ab9e3c5c776fe0793895
Lais-WHart <mmsouza@inf.ufrgs.br>
2018-03-17 03:03:07 -0300

    fix 30 repetions

--- run.py
+++ run.py
@@ -16,78 +16,78 @@ pc = [10, 20, 30, 40, 52]
 n = [1, 2, 3, 4, 5]
 
 for i in pc:
-    fft_pca_ann.run(n_modes=1, fault_prop=0.5, pcs=i * 100, repetitions=1,
+    fft_pca_ann.run(n_modes=1, fault_prop=0.5, pcs=i * 100, repetitions=35,
                     filename='fft_pca_ann_m-1_pc-' + str(i * 100),
                     batchsize=512)
 
 for i in pc:
-    fft_pca_ann.run(n_modes=2, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_ann_m-1_pc-' + str(i * 100),
+    fft_pca_ann.run(n_modes=2, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_ann_m-2_pc-' + str(i * 100),
                     batchsize=512)
 for i in pc:
-    fft_pca_ann.run(n_modes=3, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_ann_m-1_pc-' + str(i * 100),
+    fft_pca_ann.run(n_modes=3, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_ann_m-3_pc-' + str(i * 100),
                     batchsize=512)
 for i in pc:
-    fft_pca_ann.run(n_modes=5, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_ann_m-1_pc-' + str(i * 100),
+    fft_pca_ann.run(n_modes=5, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_ann_m-5_pc-' + str(i * 100),
                     batchsize=512)
 for i in pc:
-    fft_pca_ann.run(n_modes=6, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_ann_m-1_pc-' + str(i * 100),
+    fft_pca_ann.run(n_modes=6, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_ann_m-6_pc-' + str(i * 100),
                     batchsize=512)
 for i in pc:
-    fft_pca_ann.run(n_modes=7, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_ann_m-1_pc-' + str(i * 100),
+    fft_pca_ann.run(n_modes=7, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_ann_m-7_pc-' + str(i * 100),
                     batchsize=512)
 
 # --------------------fft-pca-svm----------------------------------------------------------------------------------
 for i in pc:
-    fft_pca_svm.run(n_modes=1, fault_prop=0.5, pcs=i * 100, repetitions=1,
+    fft_pca_svm.run(n_modes=1, fault_prop=0.5, pcs=i * 100, repetitions=35,
                     filename='fft_pca_svm_m-1_pc-' + str(i * 100))
 
 for i in pc:
-    fft_pca_svm.run(n_modes=2, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_svm_m-1_pc-' + str(i * 100))
+    fft_pca_svm.run(n_modes=2, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_svm_m-2_pc-' + str(i * 100))
 
 for i in pc:
-    fft_pca_svm.run(n_modes=3, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_svm_m-1_pc-' + str(i * 100))
+    fft_pca_svm.run(n_modes=3, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_svm_m-3_pc-' + str(i * 100))
 
 for i in pc:
-    fft_pca_svm.run(n_modes=5, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_svm_m-1_pc-' + str(i * 100))
+    fft_pca_svm.run(n_modes=5, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_svm_m-5_pc-' + str(i * 100))
 
 for i in pc:
-    fft_pca_svm.run(n_modes=6, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_svm_m-1_pc-' + str(i * 100))
+    fft_pca_svm.run(n_modes=6, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_svm_m-6_pc-' + str(i * 100))
 
 for i in pc:
-    fft_pca_svm.run(n_modes=7, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_svm_m-1_pc-' + str(i * 100))
+    fft_pca_svm.run(n_modes=7, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_svm_m-7_pc-' + str(i * 100))
 
 # ---------------fft-pca-Knn---------------------------------------------------------------------------------
 
 for i in pc:
-    fft_pca_knn.run(n_modes=1, fault_prop=0.5, pcs=i * 100, repetitions=1,
+    fft_pca_knn.run(n_modes=1, fault_prop=0.5, pcs=i * 100, repetitions=35,
                     filename='fft_pca_knn_m-1_pc-' + str(i * 100))
 
 for i in pc:
-    fft_pca_knn.run(n_modes=2, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_knn_m-1_pc-' + str(i * 100))
+    fft_pca_knn.run(n_modes=2, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_knn_m-2_pc-' + str(i * 100))
 
 for i in pc:
-    fft_pca_knn.run(n_modes=3, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_knn_m-1_pc-' + str(i * 100))
+    fft_pca_knn.run(n_modes=3, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_knn_m-3_pc-' + str(i * 100))
 
 for i in pc:
-    fft_pca_knn.run(n_modes=5, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_knn_m-1_pc-' + str(i * 100))
+    fft_pca_knn.run(n_modes=5, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_knn_m-5_pc-' + str(i * 100))
 
 for i in pc:
-    fft_pca_knn.run(n_modes=6, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_knn_m-1_pc-' + str(i * 100))
+    fft_pca_knn.run(n_modes=6, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_knn_m-6_pc-' + str(i * 100))
 
 for i in pc:
-    fft_pca_knn.run(n_modes=7, fault_prop=0.5, pcs=i * 100, repetitions=1,
-                    filename='fft_pca_knn_m-1_pc-' + str(i * 100))
+    fft_pca_knn.run(n_modes=7, fault_prop=0.5, pcs=i * 100, repetitions=35,
+                    filename='fft_pca_knn_m-7_pc-' + str(i * 100))
